c82458c1a7603c72b5b67ca2b13d56271d529bab
knchan0x <michael.kn.chan@gmail.com>
2022-05-13 21:34:23 +0800

    rename package, struct, functions

---
+++ internal/scraper/scraper.go
@@ -0,0 +1,329 @@
+package scraper
+
+import (
+	"bytes"
+	"crypto/tls"
+	"errors"
+	"fmt"
+	"io"
+	"net/http"
+	"strconv"
+	"strings"
+	"sync"
+	"time"
+
+	"github.com/PuerkitoBio/goquery"
+)
+
+const (
+	USER_AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36"
+	baseURL    = "https://www.bellemaison.jp/shop/commodity/0000/"
+)
+
+type Result struct {
+	ProductCode string
+	Product     *Product
+	Err         error
+}
+
+type Product struct {
+	Name   string
+	Styles []Style
+}
+
+type Style struct {
+	StyleCode string
+	ImageUrl  string
+	Colour    string
+	Size      string
+	Price     uint
+	Stock     uint
+}
+
+// Scraper
+type Scraper interface {
+	ScrapingProduct(productCode string) *Result
+	ScrapingProducts(productCodes ...string) []*Result
+}
+
+// scraper implements Crawler interface
+type scraper struct {
+	httpClient HTTPClient
+}
+
+type HTTPClient interface {
+	Do(req *http.Request) (*http.Response, error)
+}
+
+var ErrMultipleClient = errors.New("more than one http client assigned")
+
+// NewScraper return a Scraper instance,
+// default http client will be used if no httpClient provided
+func NewScraper(httpClient ...HTTPClient) (Scraper, error) {
+	if len(httpClient) > 1 {
+		return nil, ErrMultipleClient
+	}
+
+	var client HTTPClient
+	if len(httpClient) == 0 {
+		client = &http.Client{
+			Transport: &http.Transport{
+				TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+			},
+			Timeout: time.Second * 60,
+		}
+	} else {
+		client = httpClient[0]
+	}
+
+	return &scraper{httpClient: client}, nil
+}
+
+// http response
+type response struct {
+	id   string
+	data []byte
+	err  error
+}
+
+// ScrapingProducts fetches and parses multiple products from the site
+func (c *scraper) ScrapingProducts(productCodes ...string) []*Result {
+	size := len(productCodes)
+
+	if size <= 0 {
+		return nil
+	}
+
+	respCh := make(chan *response, size)
+	resultCh := make(chan *Result, size)
+	go func(respCh <-chan *response, resultCh chan<- *Result) {
+		var wg sync.WaitGroup
+		for resp := range respCh {
+			wg.Add(1)
+			go func(resp response, resultCh chan<- *Result) {
+				defer wg.Done()
+				result := Result{
+					ProductCode: resp.id,
+				}
+				if resp.err == nil {
+					result.Product, result.Err = parseHTML(resp.data)
+
+				} else {
+					result.Err = resp.err
+				}
+
+				resultCh <- &result
+			}(*resp, resultCh)
+		}
+		wg.Wait()
+		close(resultCh)
+	}(respCh, resultCh)
+
+	for _, id := range productCodes {
+		resp := response{
+			id: id,
+		}
+		data, err := fetch(c.httpClient, baseURL+id)
+		if err != nil {
+			resp.err = err
+		} else {
+			resp.data = data
+		}
+		respCh <- &resp
+	}
+	close(respCh)
+
+	results := make([]*Result, size)
+	idx := 0
+	for result := range resultCh {
+		results[idx] = result
+		idx++
+	}
+
+	return results
+}
+
+// ScrapingProduct fetches and parses product from the site
+func (c *scraper) ScrapingProduct(productCode string) *Result {
+	data, err := fetch(c.httpClient, baseURL+productCode)
+	if err != nil {
+		return &Result{
+			ProductCode: productCode,
+			Product:     nil,
+			Err:         err,
+		}
+	}
+	newProduct, err := parseHTML(data)
+	if err != nil {
+		return &Result{
+			ProductCode: productCode,
+			Product:     nil,
+			Err:         err,
+		}
+	}
+	return &Result{
+		ProductCode: productCode,
+		Product:     newProduct,
+		Err:         nil,
+	}
+}
+
+// fetch fetches web page from the site
+func fetch(httpClient HTTPClient, link string) ([]byte, error) {
+	// Set header
+	req, _ := http.NewRequest(http.MethodGet, link, nil)
+	req.Header.Set("user-agent", USER_AGENT)
+
+	// HTTP Request
+	res, err := httpClient.Do(req)
+	if err != nil {
+		return []byte{}, err
+	}
+	defer res.Body.Close()
+
+	if res.StatusCode != 200 {
+		return []byte{}, fmt.Errorf("%d: %s", res.StatusCode, res.Status)
+	}
+
+	val, _ := io.ReadAll(res.Body)
+	if err != nil {
+		return []byte{}, err
+	}
+	return val, nil
+}
+
+var (
+	PRODUCT_NOT_FOUND = errors.New("Product Not Found")
+)
+
+// parseHTML converts html page to Product struct
+func parseHTML(html []byte) (*Product, error) {
+
+	page, err := goquery.NewDocumentFromReader(bytes.NewReader(html))
+	if err != nil {
+		return nil, err
+	}
+
+	title := ""
+	page.Find("h1[class='title']").Each(func(i int, s *goquery.Selection) {
+		title = s.Text()
+	})
+
+	// product has been removed
+	if title == "お探しの商品が見つかりません" {
+		return nil, PRODUCT_NOT_FOUND
+	}
+
+	newProduct := &Product{
+		Styles: []Style{},
+	}
+
+	// get product name
+	page.Find("h1[class='product-name text-weight-bold']").Each(func(i int, s *goquery.Selection) {
+		if i == 0 {
+			newProduct.Name = s.Text()
+		}
+	})
+
+	// find the target items
+	var colour, size, current, stock, sku string
+	page.Find("#commodityStandardAreaMessage").Parent().Each(func(i int, s *goquery.Selection) {
+		s.Find(".standard-info").Each(func(i int, s *goquery.Selection) {
+			colour, _ = s.Attr("data-standard-detail2")
+
+			// check colour info
+			if colour == "-" {
+				colour = "Standard"
+			}
+
+			size, _ = s.Attr("data-standard-detail1")
+
+			// check size info
+			if size == "-" {
+				size = "Standard"
+			}
+
+			// in case it has additional size info
+			if detail, exist := s.Attr("data-standard-detail12"); exist {
+				if detail != "-" {
+					size = size + "/" + detail
+				}
+			}
+
+			current, _ = s.Attr("data-price")
+			stock, _ = s.Attr("data-stock-status")
+			sku, _ = s.Attr("data-nucleus-sku-code")
+
+			currentPrice, err := strconv.ParseUint(strings.ReplaceAll(current, ",", ""), 10, 64)
+			if err != nil {
+				currentPrice = 0
+			}
+			stockNo := parseStock(stock)
+
+			image := ""
+			s.Siblings().Find(".variation-list_item").Each(func(i int, s *goquery.Selection) {
+				s.Find("input[name='color']").Each(func(i int, s *goquery.Selection) {
+					itemColor, _ := s.Attr("data-name")
+					if colour == itemColor {
+						image, _ = s.Attr("data-img")
+					}
+				})
+			})
+
+			if image == "" {
+				s.Siblings().Find(".variation-check-radio").Each(func(i int, s *goquery.Selection) {
+					s.Find("input[name='color']").Each(func(i int, s *goquery.Selection) {
+						itemColor, _ := s.Attr("data-name")
+						if colour == itemColor {
+							image, _ = s.Attr("data-img")
+						}
+					})
+				})
+			}
+
+			if image == "" {
+				image = fmt.Sprintf("https://pic2.bellemaison.jp/shop/cms/images/0000/catalog/%s/%s_h1_001.jpg", sku[:7], sku[:7])
+			}
+
+			newStyle := Style{
+				StyleCode: sku[7:],
+				ImageUrl:  image,
+				Colour:    colour,
+				Size:      size,
+				Price:     uint(currentPrice),
+				Stock:     stockNo,
+			}
+			newProduct.Styles = append(newProduct.Styles, newStyle)
+		})
+	})
+
+	return newProduct, nil
+}
+
+func parseStock(description string) (stock uint) {
+	switch description {
+	case "在庫あり":
+		stock = 99
+	case "売り切れ":
+		fallthrough
+	case "販売停止":
+		fallthrough
+	case "売り切れ（再入荷なし）":
+		stock = 0
+	default:
+		if strings.Contains(description, "在庫：") {
+			temp := strings.Split(description, "：")
+			tempNo, err := strconv.ParseUint(temp[1], 10, 64)
+			if err != nil {
+				stock = 0
+			} else {
+				stock = uint(tempNo)
+			}
+		}
+
+		if strings.Contains(description, "入荷予定") {
+			stock = 0
+		}
+	}
+	return stock
+}
---
+++ internal/scraper/scraper_test.go
@@ -0,0 +1,254 @@
+package scraper
+
+import (
+	"bytes"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"reflect"
+	"testing"
+)
+
+type MockClient struct {
+	DoFunc func(req *http.Request) (*http.Response, error)
+}
+
+func (m *MockClient) Do(req *http.Request) (*http.Response, error) {
+	if m.DoFunc != nil {
+		return m.DoFunc(req)
+	}
+	return &http.Response{}, nil
+}
+
+func TestNewCrawler(t *testing.T) {
+	c, err := NewScraper()
+	if c == nil || err != nil {
+		t.Errorf("initializing crawler failed: %v", err)
+	}
+	c, err = NewScraper(&http.Client{})
+	if c == nil || err != nil {
+		t.Errorf("initializing crawler failed: %v", err)
+	}
+	c, err = NewScraper(&http.Client{}, &http.Client{})
+	if c != nil || err != ErrMultipleClient {
+		t.Errorf("failed to prevent multiple http clients")
+	}
+}
+
+func TestRetrieveProduct_PageNotFound(t *testing.T) {
+	client := &MockClient{
+		DoFunc: func(req *http.Request) (*http.Response, error) {
+			return &http.Response{
+				StatusCode: http.StatusBadRequest,
+				Body:       io.NopCloser(bytes.NewReader([]byte{})),
+			}, nil
+		},
+	}
+	c, err := NewScraper(client)
+	if c == nil || err != nil {
+		t.Errorf("initializing crawler failed: %v", err)
+	}
+
+	r := c.ScrapingProduct("1000000")
+	if r.Err == nil {
+		t.Errorf("Err != nil when page is not available")
+	}
+}
+
+func TestRetrieveProduct_ProductNotFound(t *testing.T) {
+	client := getMockClientwithFile("./test/failed.html")
+	c, err := NewScraper(client)
+	if c == nil || err != nil {
+		t.Errorf("initializing crawler failed: %v", err)
+	}
+
+	r := c.ScrapingProduct("1000000")
+	if r.Err == nil {
+		t.Errorf("Should not be no error")
+	}
+	if r.Err != PRODUCT_NOT_FOUND {
+		t.Errorf("Found product that should be not available")
+	}
+}
+
+func TestRetrieveProduct_Success(t *testing.T) {
+	client := getMockClientwithFile("./test/success.html")
+	c, err := NewScraper(client)
+	if c == nil || err != nil {
+		t.Errorf("initializing crawler failed: %v", err)
+	}
+
+	r := c.ScrapingProduct(mockResult.ProductCode)
+	if r.ProductCode != mockResult.ProductCode {
+		t.Errorf("incorrect product code")
+	}
+	if r.Err != nil {
+		t.Errorf("Error: %v", err)
+	}
+	if r.Product.Name != mockResult.Product.Name {
+		t.Errorf("Error: %v", err)
+	}
+	for i, mockStyle := range mockResult.Product.Styles {
+		if r.Product.Styles[i].StyleCode != mockStyle.StyleCode {
+			t.Errorf("style not match")
+		}
+		if r.Product.Styles[i].ImageUrl != mockStyle.ImageUrl {
+			t.Errorf("imageUrl not match")
+		}
+		if r.Product.Styles[i].Colour != mockStyle.Colour {
+			t.Errorf("colour not match")
+		}
+		if r.Product.Styles[i].Size != mockStyle.Size {
+			t.Errorf("size not match")
+		}
+		if r.Product.Styles[i].Price != mockStyle.Price {
+			t.Errorf("price not match")
+		}
+		if r.Product.Styles[i].Stock != mockStyle.Stock {
+			t.Errorf("stock not match")
+		}
+	}
+}
+
+func getMockClientwithFile(name string) *MockClient {
+	getMockHtmlPage()
+
+	file, err := os.ReadFile(name)
+	if err != nil {
+		log.Panic("test file not available")
+	}
+
+	client := &MockClient{
+		DoFunc: func(req *http.Request) (*http.Response, error) {
+			return &http.Response{
+				StatusCode: http.StatusOK,
+				Body:       io.NopCloser(bytes.NewReader(file)),
+			}, nil
+		},
+	}
+	return client
+}
+
+// need to match with the html file downloaded (./test/success.html)
+var mockResult = &Result{
+	ProductCode: "1129250",
+	Product: &Product{
+		Name: "シートマッサージャー",
+		Styles: []Style{
+			{StyleCode: "01001", ImageUrl: "https://pic2.bellemaison.jp/shop/cms/images/0000/catalog/1129250/1129250_h1_001.jpg", Colour: "Standard", Size: "Standard", Price: 6578, Stock: 4},
+		},
+	},
+	Err: nil,
+}
+
+func getMockHtmlPage() {
+	if err := download("./test/success.html", "https://www.bellemaison.jp/shop/commodity/0000/1129250"); err != nil {
+		panic(err)
+	}
+
+	if err := download("./test/failed.html", "https://www.bellemaison.jp/shop/commodity/0000/1000000"); err != nil {
+		panic(err)
+	}
+}
+
+func download(filepath string, url string) error {
+
+	// get data
+	resp, err := http.Get(url)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	// create file
+	out, err := os.Create(filepath)
+	if err != nil {
+		return err
+	}
+	defer out.Close()
+
+	// write to file
+	_, err = io.Copy(out, resp.Body)
+	return err
+}
+
+func TestNewScraper(t *testing.T) {
+	type args struct {
+		httpClient []HTTPClient
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    Scraper
+		wantErr bool
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := NewScraper(tt.args.httpClient...)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("NewScraper() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("NewScraper() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_scraper_ScrapingProducts(t *testing.T) {
+	type fields struct {
+		httpClient HTTPClient
+	}
+	type args struct {
+		productCodes []string
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		args   args
+		want   []*Result
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			c := &scraper{
+				httpClient: tt.fields.httpClient,
+			}
+			if got := c.ScrapingProducts(tt.args.productCodes...); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("scraper.ScrapingProducts() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_scraper_ScrapingProduct(t *testing.T) {
+	type fields struct {
+		httpClient HTTPClient
+	}
+	type args struct {
+		productCode string
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		args   args
+		want   *Result
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			c := &scraper{
+				httpClient: tt.fields.httpClient,
+			}
+			if got := c.ScrapingProduct(tt.args.productCode); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("scraper.ScrapingProduct() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
