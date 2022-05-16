60930502b34b1be71dc0feadc2bb9cb244757fce
knchan0x <michael.kn.chan@gmail.com>
2022-05-16 21:37:31 +0800

    move to cmd/web/page

---
+++ internal/config/config_test.go
@@ -0,0 +1,19 @@
+package config
+
+import "testing"
+
+func TestLoadConfig(t *testing.T) {
+	tests := []struct {
+		name    string
+		wantErr bool
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if err := LoadConfig(); (err != nil) != tt.wantErr {
+				t.Errorf("LoadConfig() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
